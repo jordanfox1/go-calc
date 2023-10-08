2fc789115c8a88726e338b19ee0f264fe1f16747
Jordan Fox <jordan.fox@canstar.com.au>
2023-10-08 21:13:23 +1000

    add function

--- calc/calc.go
+++ calc/calc.go
@@ -1,6 +1,10 @@
 package calc
 
-import "strings"
+import (
+	"log"
+	"strconv"
+	"strings"
+)
 
 func DetermineOperation(input string) string {
 	if strings.Contains(input, "+") {
@@ -16,4 +20,27 @@ func DetermineOperation(input string) string {
 		return "divide"
 	}
 	return "invalid"
+}
+
+func Calculate(input string, operation string) int {
+	input = strings.ReplaceAll(input, " ", "")
+	if operation == "add" {
+		nums := strings.Split(input, "+")
+
+		if len(nums) != 2 {
+			log.Fatal("Invalid input. There should be exactly two numbers separated by '+'.")
+		}
+
+		num1, err1 := strconv.Atoi(nums[0])
+		num2, err2 := strconv.Atoi(nums[1])
+
+		if err1 != nil || err2 != nil {
+			log.Fatal("Invalid input. Both operands should be valid integers.")
+		}
+
+		result := num1 + num2
+
+		return result
+	}
+	return 0
 }
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -1,6 +1,8 @@
 package calc
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestDetermineOperation(t *testing.T) {
 	type args struct {
@@ -51,6 +53,41 @@ func TestDetermineOperation(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := DetermineOperation(tt.args.input); got != tt.want {
 				t.Errorf("DetermineOperation() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalculate(t *testing.T) {
+	type args struct {
+		input     string
+		operation string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test Add",
+			args: args{"1 + 49", "add"},
+			want: 50,
+		},
+		{
+			name: "Test Add",
+			args: args{"294724 + 324284", "add"},
+			want: 619008,
+		},
+		{
+			name: "Test Add",
+			args: args{"1 + 1", "add"},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Calculate(tt.args.input, tt.args.operation); got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
 			}
 		})
 	}
